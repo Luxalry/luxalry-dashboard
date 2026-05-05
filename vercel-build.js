ef254f3e9236e43b98614e6b5e301e06eb47fb3f
Luxalry <luxalry.benoua@gmail.com>
2026-05-05 20:09:28 +0100

    Update vercel-build.js

--- vercel-build.js
+++ vercel-build.js
@@ -1,7 +1,6 @@
 {
   "version": 2,
   "outputDirectory": ".",
-  "buildCommand": "node vercel-build.js",
   "rewrites": [
     { "source": "/admin", "destination": "/admin/login.html" },
     { "source": "/admin/dashboard", "destination": "/admin/dashboard.html" },
@@ -11,14 +10,8 @@
     {
       "source": "/admin/(.*)",
       "headers": [
-        {
-          "key": "X-Frame-Options",
-          "value": "DENY"
-        },
-        {
-          "key": "X-Content-Type-Options",
-          "value": "nosniff"
-        },
+        { "key": "X-Frame-Options", "value": "DENY" },
+        { "key": "X-Content-Type-Options", "value": "nosniff" },
         {
           "key": "Content-Security-Policy",
           "value": "default-src *; script-src * 'unsafe-inline' 'unsafe-eval'; style-src * 'unsafe-inline'; img-src * data:; font-src *; connect-src *; frame-src *;"
@@ -28,10 +21,7 @@
     {
       "source": "/admin/dashboard-mini.js",
       "headers": [
-        {
-          "key": "Cache-Control",
-          "value": "no-store"
-        }
+        { "key": "Cache-Control", "value": "no-store" }
       ]
     }
   ]
